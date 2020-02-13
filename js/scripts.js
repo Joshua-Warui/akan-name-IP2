a2e4d1c32e60029d5cd10b3bd9bfbf8146f22d01
joshua warui <josh.w.warui52@gmail.com>
2020-02-13 15:19:00 +0300

    add an array to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var female = ["Akosua","Adwoa","Abena","Akua"]
+var female = ["Akosua","Adwoa","Abena","Akua","Yaa","Afua","Ama"];
+
+function akanName(CC,YY,MM,DD){
+  var arrayPosition = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
+  console.log(arrayPosition);
+  var name = male[arrayPosition];
+  console.log(name);
+}
+
+akanName(19,96.08,19);
