72ea1e1655a20ae9df2716341296c87d7b7ede9a
joshua warui <josh.w.warui52@gmail.com>
2020-02-14 13:46:48 +0300

    fix formula to return positive values only

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 function akanName(CC,YY,MM,DD){
+  var date = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"]
 	var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
   var female = ["Akosua","Adwoa","Abena","Akua","Yaa","Afua","Ama"];
   
+
   var DD = parseInt(document.getElementById("day").value);
   var MM = parseInt(document.getElementById("month").value);
   var year = document.getElementById("year").value;
   var YY = parseInt(year.slice(2,4));
   var CC = parseInt(year.slice(0,2));
   var gender = document.getElementById("gender").value;
+  
 
-
-  var arrayPosition = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-  arrayPosition = parseInt(arrayPosition); 
-  console.log(arrayPosition);
+  var arrayPosition = Math.abs((((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7);
+  arrayPosition = Math.floor(arrayPosition);
+  console.log(arrayPosition); 
 
   if(gender === "male"){
     sex = male;
@@ -21,6 +23,9 @@ function akanName(CC,YY,MM,DD){
   };
 
   var name = sex[arrayPosition];
+  date = date[arrayPosition];
 
-  document.getElementById("akan-name").innerHTML = "Your Akan name is " + name + ".";
+  console.log(date);
+  
+  document.getElementById("akan-name").innerHTML = "Your Akan name is " + name + " and you were born on a " + date + ".";
 };
