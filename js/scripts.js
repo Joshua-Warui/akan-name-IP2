db7e646e19979b88a140926bbebe063dccc980d9
joshua warui <josh.w.warui52@gmail.com>
2020-02-17 11:29:53 +0300

    add old formula to calculate akan name and day to master branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
-function akanName(CC,YY,MM,DD){
+function akanName(){
   var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
 	var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
   var female = ["Akosua","Adwoa","Abena","Akua","Yaa","Afua","Ama"];
-  var date = parseInt(document.getElementById("day").value);
-  var month = parseInt(document.getElementById("month").value);
-  var year = parseInt(document.getElementById("year").value);
+  var DD = parseInt(document.getElementById("day").value);
+  var MM = parseInt(document.getElementById("month").value);
+  var year = document.getElementById("year").value;
+  var YY = parseInt(year.slice(2,4));
+  var CC = parseInt(year.slice(0,2));
   var gender = document.getElementById("gender").value;
-  var arrayPosition = new Date(`${year}-${month}-${date}`);
-  arrayPosition = arrayPosition.getDay();
-  console.log(date);
-  
-  if (date < 1 || date >31 || date !== date){
+  var arrayPosition = Math.abs((((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7);
+  arrayPosition = Math.floor(arrayPosition);
+  if (DD < 1 || DD >31 || DD !== DD){
     alert("Type a date between 01 and 31")
-  } else if(month <1 || month>12 || month !== month){
+  } else if(MM <1 || MM>12 || MM !== MM){
     alert("Type a month between 01 and 12")
   }else{
     console.log("DOB seems fine");
