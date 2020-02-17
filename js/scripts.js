698a1d9a51668648847f3ba44a0bf84974680a44
joshua warui <josh.w.warui52@gmail.com>
2020-02-17 09:24:52 +0300

    change formula for calculating index(arrayPosition)

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
 function akanName(CC,YY,MM,DD){
-  var date = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
+  var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
 	var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
   var female = ["Akosua","Adwoa","Abena","Akua","Yaa","Afua","Ama"];
-  var DD = parseInt(document.getElementById("day").value);
-  var MM = parseInt(document.getElementById("month").value);
-  var year = document.getElementById("year").value;
-  var YY = parseInt(year.slice(2,4));
-  var CC = parseInt(year.slice(0,2));
+  var date = parseInt(document.getElementById("day").value);
+  var month = parseInt(document.getElementById("month").value);
+  var year = parseInt(document.getElementById("year").value);
   var gender = document.getElementById("gender").value;
-  var arrayPosition = Math.abs((((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7);
-  arrayPosition = Math.floor(arrayPosition);
-  console.log(arrayPosition); 
+  var arrayPosition = new Date(`${year}-${month}-${date}`)
+  arrayPosition = arrayPosition.getDay();
+  var sex
   if(gender === "male"){
     sex = male;
   } else {
     sex = female;
   };
   var name = sex[arrayPosition];
-  date = date[arrayPosition];
-  document.getElementById("akan-name").innerHTML = "Your Akan name is " + name + " and you were born on a " + date + ".";
+  day = day[arrayPosition];
+  document.getElementById("akan-name").innerHTML = "Your Akan name is " + name + " and you were born on a " + day + ".";
 };
